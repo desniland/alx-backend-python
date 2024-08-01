85d9dbd83d7fe152cf34737d2298f6cc851865fc
desniland <albertdesmond26@gmail.com>
2024-08-01 11:22:24 +0300

    Basic Annotation, type casting

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+a module that provides a function for converting a float to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    It converts a float to a string and returns the result.
+
+    Parameters:
+    n (float): The float to convert.
+
+    Returns:
+    str: The string representation of n.
+    """
+    return str(n)
